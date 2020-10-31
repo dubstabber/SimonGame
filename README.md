f8d5bd8a683f190bd27967f34c96f4ff7ee6bfd3
Dubstabber <taesan@protonmail.com>
2020-10-31 22:18:15 +0100

    Implement click detection and pattern validation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Simon Game
+##### Watch and remember blinking blocks then repeat the whole pattern.
+##### It is a simple game that works in the browser. Just open the index.html to begin.
+
--- index.js
+++ index.js
@@ -1,52 +1,80 @@
-let level = 1;
-let pattern = [];
-let userPattern = [];
+let level;
+let pattern;
+let userPattern;
 let started = false;
 let error = false;
 
 $(document).on("keypress",() => {
     if(!started) {
-        $("#level-title").html("Level "+ level);
         started = true;
+        error  = false;
+        level = 0;
+        pattern = [];
+        userPattern = [];
         newBlock();
     }
 });
 
 $("#green").click(() => {
-    checkBlocks(0);
+    if(started){
+        new Audio("./sounds/green.mp3").play();
+        checkBlocks(0, "#green");
+    }
 })
 
 $("#red").click(() => {
-    checkBlocks(1);
+    if(started){
+        new Audio("./sounds/red.mp3").play();
+        checkBlocks(1, "#red");
+    }
 })
 
 $("#yellow").click(() => {
-    checkBlocks(2);
+    if(started){
+        new Audio("./sounds/yellow.mp3").play();
+        checkBlocks(2, "#yellow");
+    }
 })
 
 $("#blue").click(() => {
-    checkBlocks(3);
+    if(started){
+        new Audio("./sounds/blue.mp3").play();
+        checkBlocks(3, "#blue");
+    }
 })
 
-function checkBlocks(id) {
+function checkBlocks(id, color) {
     userPattern.push(id);
+    $(color).addClass("pressed");
 
-    for(let i = 0; i < pattern.length; i++){
+    for(let i = 0; i < userPattern.length; i++){
         if(pattern[i] !== userPattern[i])
             error = true;
     }
 
     if(error){
         new Audio("./sounds/wrong.mp3").play();
-    } else {
-        level++;
+        $("#level-title").html("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        },100);
+        started = false;
+    } else if(pattern.length === userPattern.length) {
+        userPattern = [];
         setTimeout(newBlock, 700);
     }
+
+    setTimeout(() => {
+        $(color).removeClass("pressed")
+    }, 100);
 }
 
 function newBlock() {
     let randomBlock = Math.floor(Math.random() * 4);
     pattern.push(randomBlock);
+    level++;
+    $("#level-title").html("Level "+ level);
 
     switch(randomBlock){
         case 0:
